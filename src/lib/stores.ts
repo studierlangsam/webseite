e9bb13510bc32739b1a00a2639b9bae22d859604
Dominik Rosch <dominik.rosch@student.kit.edu>
2025-10-13 17:50:01 +0200

    translation proof of concept

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,4 @@
 import { writable } from "svelte/store";
 
 export let visibleSectionHash = writable("");
+export const language = writable({ german: false });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,44 @@
 <script>
+	import Tutors from '$lib/tutors/Tutors.svelte';
+	import Section from '../lib/Section.svelte';
+	import Wochenplan from '../lib/Wochenplan/Wochenplan.svelte';
+	import Faq from '../lib/faq/FAQ.svelte';
+	import { language } from '../lib/stores';
 
-	import Tutors from "$lib/tutors/Tutors.svelte";
-	import Section from "../lib/Section.svelte";
-	import Wochenplan from "../lib/Wochenplan/Wochenplan.svelte";
-	import Faq from "../lib/faq/FAQ.svelte";
-
-    export let data;
+	export let data;
 </script>
 
 <!-- <Section address=""> -->
-<h1>Hallo du!</h1>
-<p>Willkommen bei Studier Langsam!</p>
-<p>
-    Bei uns erwartet dich eine abwechslungsreiche O-Phase mit Spiele- sowie Cocktailabend, 
-    gemeinsamem Essen und weiteren spannenden Aktivitäten in Karlsruhe und um den Campus. 
-    Dabei lernst du deine zukünftigen Mitstudierenden kennen und kannst hier neue Freund:innen finden. 
-    Auch nach der O-Phase treffen wir uns regelmäßig zu Stammtischen, die wir mit Spielen, Gesprächen, Diskussionen und schlechten Wortwitzen verbringen. 
-    Aber vor allem: Komm zu uns, denn wir haben Kekse! 🍪
-</p>
-<p>
-    Auf dieser Seite findest du Informationen über uns, die O-Phase und das Studium am KIT.
-</p>
+{#if $language.german}
+	<h1>Hallo du!</h1>
+	<p>Willkommen bei Studier Langsam!</p>
+	<p>
+		Bei uns erwartet dich eine abwechslungsreiche O-Phase mit Spiele- sowie Cocktailabend,
+		gemeinsamem Essen und weiteren spannenden Aktivitäten in Karlsruhe und um den Campus. Dabei
+		lernst du deine zukünftigen Mitstudierenden kennen und kannst hier neue Freund:innen finden.
+		Auch nach der O-Phase treffen wir uns regelmäßig zu Stammtischen, die wir mit Spielen,
+		Gesprächen, Diskussionen und schlechten Wortwitzen verbringen. Aber vor allem: Komm zu uns, denn
+		wir haben Kekse! 🍪
+	</p>
+	<p>Auf dieser Seite findest du Informationen über uns, die O-Phase und das Studium am KIT.</p>
+{:else}
+	<h1>Hello there!</h1>
+	<p>Welcome to Studier Langsam (Study Slowly)!</p>
+	<p>
+		Bei uns erwartet dich eine abwechslungsreiche O-Phase mit Spiele- sowie Cocktailabend,
+		gemeinsamem Essen und weiteren spannenden Aktivitäten in Karlsruhe und um den Campus. Dabei
+		lernst du deine zukünftigen Mitstudierenden kennen und kannst hier neue Freund:innen finden.
+		Auch nach der O-Phase treffen wir uns regelmäßig zu Stammtischen, die wir mit Spielen,
+		Gesprächen, Diskussionen und schlechten Wortwitzen verbringen. Aber vor allem: Komm zu uns, denn
+		wir haben Kekse! 🍪
+	</p>
+	<p>Auf dieser Seite findest du Informationen über uns, die O-Phase und das Studium am KIT.</p>
+{/if}
 <!-- </Section> -->
-<Wochenplan wochenplan={data.wochenplan}/>
-<Tutors tutors={data.tutors}/>
-<Faq faq={data.faq}/>
+<Wochenplan wochenplan={data.wochenplan} />
+<Tutors tutors={data.tutors} />
+<Faq faq={data.faq} />
 
 <style lang="scss">
-@use '$style/base';
+	@use '$style/base';
 </style>
