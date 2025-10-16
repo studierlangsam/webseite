4f33c82c67757453a1a45a0ae9cdfbae4afce712
J-A <uvepb@student.kit.edu>
2025-10-16 13:21:50 +0200

    Better solution to translate tutors page

--- src/lib/tutors/Tutors.svelte
+++ src/lib/tutors/Tutors.svelte
@@ -6,32 +6,25 @@
     export let tutors: T[];
 </script>
 
-{#if $language.german}
-    <Section address="Tutor:innen">
+<Section address={$language.german ? "Tutor:innen" : "Tutors"}>
+    {#if $language.german}
         <p>
             Wir sind die Menschen, die dich durch deine O-Phase begleiten werden. 
-            Falls du irgendwelche Fragen hast, kannst du uns jederzeit eine Email an 
+            Falls du irgendwelche Fragen hast, kannst du uns jederzeit eine Email an
             <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a> schreiben.
         </p>
-    <div class="tutors">
-    {#each tutors as tutor}
-        <Tutor {tutor} />
-    {/each}
-    </div>
-    </Section>
-{:else}
-    <Section address="Tutors">
+    {:else}
         <p>
-            We're the people who will guide you through your O-Phase. If you have any questions,
-            you can always write an email to <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a>.
+        We're the people who will guide you through your O-Phase. If you have any questions,
+        you can always write an email to <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a>.
         </p>
-    <div class="tutors">
-    {#each tutors as tutor}
-        <Tutor {tutor} />
-    {/each}
-    </div>
-    </Section>
-{/if}
+    {/if}
+<div class="tutors">
+{#each tutors as tutor}
+    <Tutor {tutor} />
+{/each}
+</div>
+</Section>
 <style lang="scss">
 @use "$style/base";
 </style>
