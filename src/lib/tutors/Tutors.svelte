d987a54b188356d625edca0ae2ec97eca583158e
J-A <uvepb@student.kit.edu>
2025-10-16 12:41:51 +0200

    Translate tutor description text

--- src/lib/tutors/Tutors.svelte
+++ src/lib/tutors/Tutors.svelte
@@ -2,15 +2,23 @@
 	import Section from "../Section.svelte";
 	import Tutor from "./Tutor.svelte";
     import type {Tutor as T} from "./tutors.ts";
+    import { language } from '../stores';
     export let tutors: T[];
 </script>
 
 <Section address="Tutor:innen">
-<p>
-    Wir sind die Menschen, die dich durch deine O-Phase begleiten werden. 
-    Falls du irgendwelche Fragen hast, kannst du uns jederzeit eine Email an 
-    <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a> schreiben.
-</p>
+{#if $language.german}
+    <p>
+        Wir sind die Menschen, die dich durch deine O-Phase begleiten werden. 
+        Falls du irgendwelche Fragen hast, kannst du uns jederzeit eine Email an 
+        <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a> schreiben.
+    </p>
+{:else}
+    <p>
+        We're the people who will guide you through your O-Phase. If you have any questions,
+        you can always write an email to <a href="mailto:tutoren@studierlangsam.de">tutoren@studierlangsam.de</a>.
+    </p>
+{/if}
 <div class="tutors">
 {#each tutors as tutor}
     <Tutor {tutor} />
